8be1beaef3f5bc41fa30a5bf9e61ee5f986449c0
dzejeu <michal.swat94@gmail.com>
2017-09-30 18:25:31 +0200

    added class for searching yt videos by band name, added class for handling videos playing in webbrowser

--- mixer/youtube/searching.py
+++ mixer/youtube/searching.py
@@ -7,8 +7,7 @@ QueryOptions = namedtuple('QueryOptions', ['band', 'max_results'])
 QueryResult = namedtuple('QueryResult', ['song_name', 'song_id'])
 
 
-class YouTubeMusicSeeker:
-
+class YouTubeClient:
     # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
     # tab of
     #   https://cloud.google.com/console
@@ -17,32 +16,50 @@ class YouTubeMusicSeeker:
         self._DEVELOPER_KEY = "AIzaSyCG6uGNl6FKqwXqszqeEttXVNFiuieOWzo"
         self._YOUTUBE_API_SERVICE_NAME = "youtube"
         self._YOUTUBE_API_VERSION = "v3"
+        self.client = build(self._YOUTUBE_API_SERVICE_NAME, self._YOUTUBE_API_VERSION,
+                        developerKey=self._DEVELOPER_KEY)
+
+    def get_video_duration(self, video_id):
+        """
+        :param video_id: unique id of yt video
+        :return: duration in seconds
+        """
+        response = self.client.videos().list(
+            part='contentDetails',
+            id=video_id,
+            maxResults=1
+        ).execute()
+
+        duration = response['items'][0]['contentDetails']['duration']
+
+        # convert duration from ISO 8601 str to integer amount of seconds
+
+        duration = duration.replace('PT', '').replace('M', ':').replace('S', '')
+        minutes, seconds = duration.split(':')
+        return int(minutes)*60 + int(seconds)
 
     def youtube_search(self, options):
         """
         :param options: Namedtuple with band name and maximum amount of search results
         :return: list of Namedtuples (song_name, song_id)
         """
-        youtube = build(self._YOUTUBE_API_SERVICE_NAME, self._YOUTUBE_API_VERSION,
-                        developerKey=self._DEVELOPER_KEY)
 
         # Call the search.list method to retrieve results matching the specified
         # query term.
-        search_response = youtube.search().list(
+        search_response = self.client.search().list(
             q=options.band,
             part="id,snippet",
-            maxResults=options.max_results
+            maxResults=options.max_results,
+            type='video'
         ).execute()
 
-        results = []
-
-        for search_result in search_response.get("items", []):
-            if search_result["id"]["kind"] == "youtube#video":
-                results.append(QueryResult(search_result["snippet"]["title"], search_result["id"]["videoId"]))
+        results = [QueryResult(search_result["snippet"]["title"], search_result["id"]["videoId"]) for search_result in
+                   search_response.get("items", [])]
 
         return results
 
+
 if __name__ == "__main__":
-    seeker = YouTubeMusicSeeker()
-    for el in seeker.youtube_search(QueryOptions('REM', 5)):
-        print el
+    seeker = YouTubeClient()
+    id = seeker.youtube_search(QueryOptions('REM', 5))[0].song_id
+    print seeker.get_video_duration(id)
